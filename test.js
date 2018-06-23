d0e9bbdc67cb8fa862cd3c709d04c85e120f2ae2
antgod <m18513622209@163.com>
2018-06-23 12:40:41 +0800

    add

---
+++ test.js
@@ -0,0 +1,8 @@
+function helloWorld(person) {
+    return 'Hello, ' + person;
+}
+var u = 1;
+var user = null;
+console.log(helloWorld(user));
+var myFavoriteNumber = 'seven';
+myFavoriteNumber = 7;
---
+++ test.ts
@@ -0,0 +1,9 @@
+function helloWorld(person) {
+  return 'Hello, ' + person;
+}
+const u = 1
+const user = null
+console.log(helloWorld(user));
+
+let myFavoriteNumber = 'seven';
+myFavoriteNumber = 7;
